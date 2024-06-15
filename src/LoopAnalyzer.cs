76e26c8a2fda77f667129994e036b0fca254bf09
Marco Carvalho <marcolucio27@gmail.com>
2024-06-15 11:32:12 -0300

    remove "forStatement" because it's not working properly

--- src/LoopAnalyzer.cs
+++ src/LoopAnalyzer.cs
@@ -4,17 +4,21 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
+namespace LoopAnalyzer;
+
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class LoopAnalyzer : DiagnosticAnalyzer
 {
-    public const string DiagnosticId = "LoopAnalyzer";
-    private static readonly LocalizableString Title = "Loop usage recommendation";
-    private static readonly LocalizableString MessageFormat = "Consider using {0} loop for {1}";
-    private const string Category = "Performance";
+    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+        id: "LoopAnalyzer",
+        title: "Loop usage recommendation",
+        messageFormat: "Consider using '{0}' loop when iterating over a '{1}'",
+        category: "Performance",
+        defaultSeverity: DiagnosticSeverity.Warning,
+        isEnabledByDefault: true
+    );
 
-    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
-
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [Rule];
 
     public override void Initialize(AnalysisContext context)
     {
@@ -25,34 +29,24 @@ public class LoopAnalyzer : DiagnosticAnalyzer
 
     private static void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
     {
-        var node = context.Node;
-        var semanticModel = context.SemanticModel;
-
-        switch (node)
+        switch (context.Node)
         {
-            case ForStatementSyntax forStatement:
+            case ForEachStatementSyntax forEachStatement:
                 {
-                    var info = semanticModel.GetTypeInfo(forStatement.Declaration.Type);
+                    var info = context.SemanticModel.GetTypeInfo(forEachStatement.Expression);
 
-                    if (info.Type != null && info.Type.OriginalDefinition.ToString() == "System.Collections.Generic.List<T>")
+                    if (info.Type != null && info.Type.TypeKind == TypeKind.Array)
                     {
-                        var diagnostic = Diagnostic.Create(Rule, forStatement.GetLocation(), "foreach", "List");
+                        var diagnostic = Diagnostic.Create(Rule, forEachStatement.GetLocation(), "for", "Array");
                         context.ReportDiagnostic(diagnostic);
                     }
 
                     break;
                 }
 
-            case ForEachStatementSyntax forEachStatement:
+            case ForStatementSyntax forStatement:
                 {
-                    var info = semanticModel.GetTypeInfo(forEachStatement.Expression);
-
-                    if (info.Type != null && info.Type.TypeKind == TypeKind.Array)
-                    {
-                        var diagnostic = Diagnostic.Create(Rule, forEachStatement.GetLocation(), "for", "array");
-                        context.ReportDiagnostic(diagnostic);
-                    }
-
+                    //TODO
                     break;
                 }
         }
